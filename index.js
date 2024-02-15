abf3ba2f660cf310cda7ac5c8e3a8559d7c91fea
kexinsun82 <kexin.sun82@gmail.com>
2024-02-15 09:15:03 -0500

    loading progress bar and going to login page

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var i = 0;
+
 function move() {
+  resetProgressBar();
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("myBar");
     var width = 1;
     var id = setInterval(frame, 10);
+
     function frame() {
       if (width >= 100) {
         clearInterval(id);
@@ -16,3 +19,38 @@ function move() {
     }
   }
 }
+
+// progress bar
+document.addEventListener("DOMContentLoaded", function () {
+
+  var avatarButton = document.getElementById("user-avatar");
+
+  avatarButton.addEventListener("click", function () {
+    resetProgressBar();
+    simulateProgress();
+  });
+
+  function simulateProgress() {
+    let progress = 0;
+
+    const interval = setInterval(function () {
+      progress += 1;
+      document.getElementById("myBar").style.width = progress + "%";
+
+      if (progress >= 100) {
+        clearInterval(interval);
+        navigateToNextPage();
+      }
+
+    }, 10);
+  }
+
+  function resetProgressBar() {
+    document.getElementById("myBar").style.width = "0%";
+  }
+
+  function navigateToNextPage() {
+    var nextPageURL = "./login.html";
+    window.location.href = nextPageURL;
+  }
+});
