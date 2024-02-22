200cf67f98c72305695ee8a8461e324d66f61c65
kexinsun82 <kexin.sun82@gmail.com>
2024-02-22 17:58:14 -0500

    modified usersection.js to fixed one bug

--- usersection.js
+++ usersection.js
@@ -5,7 +5,7 @@ localStorage.removeItem("playername")
 function onNameSubmit(event) {
     event.preventDefault();
     var playerName = document.getElementById('playerName').value;
-    window.location = "/Users/patelchintan/Desktop/Web Dev Second/Studio 2/Studio Practice/section.html";
+    window.location = "section.html";
     console.log(playerName);
     localStorage.setItem("playername", playerName);
 }
