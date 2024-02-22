a7e8b75b474f17b02d1b461876d9245cde3669c9
ujjwalahuja28 <uahuja08@gmail.com>
2024-02-22 15:22:12 -0500

    section.html page created which includes level selection

--- section.js
+++ section.js
@@ -1,17 +1,77 @@
-// console.log("This is the score from easy",localStorage.getItem("easyscore"))
+// // console.log("This is the score from easy",localStorage.getItem("easyscore"))
 
-let easyscore = localStorage.getItem("easyscore")
-let meduimscore = localStorage.getItem("mediumscore")
+// let easyscore = localStorage.getItem("easyscore")
+// let meduimscore = localStorage.getItem("mediumscore")
 
-if(easyscore){
-    document.getElementById('easytrophy').style.display = "flex"
+// if(easyscore){
+//     document.getElementById('easytrophy').style.display = "flex"
+// }
+// else{
+//    document.getElementById('easytrophy').style.display = "none"
+// }
+// if(meduimscore){
+//     document.getElementById('mediumtrophy').style.display = "flex"
+// }
+// else{
+// document.getElementById('mediumtrophy').style.display = "none"
+// }
+
+
+
+
+var i = 0;
+
+function move() {
+  resetProgressBar();
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
 }
-else{
-   document.getElementById('easytrophy').style.display = "none"
-}
-if(meduimscore){
-    document.getElementById('mediumtrophy').style.display = "flex"
-}
-else{
-document.getElementById('mediumtrophy').style.display = "none"
-}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  var avatarButton = document.getElementById("user-avatar");
+
+  avatarButton.addEventListener("click", function () {
+    resetProgressBar();
+    simulateProgress();
+  });
+
+  function simulateProgress() {
+    let progress = 0;
+
+    const interval = setInterval(function () {
+      progress += 1;
+      document.getElementById("myBar").style.width = progress + "%";
+
+      if (progress >= 100) {
+        clearInterval(interval);
+        navigateToNextPage();
+      }
+
+    }, 10);
+  }
+
+  function resetProgressBar() {
+    document.getElementById("myBar").style.width = "0%";
+  }
+
+  function navigateToNextPage() {
+    var nextPageURL = "./login.html";
+    window.location.href = nextPageURL;
+  }
+});
